5d42ab5383df6c3a8c0521399e2f3ecd0266c10f
Pedro Matheus <92803869+pedrojoop@users.noreply.github.com>
2023-11-19 18:54:05 -0300

    Segunda tarefa

---
+++ PrimeiroProjeto/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package PrimeiroProjeto;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        String name = "Pedro";
+        System.out.println("Hello " + name);
+    }
+}
