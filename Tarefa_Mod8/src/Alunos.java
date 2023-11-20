bdf256e2016bd47b9e4a48a1f3d98485db1181c4
Pedro Matheus <92803869+pedrojoop@users.noreply.github.com>
2023-11-20 12:03:50 -0300

    Quinta tarefa

--- Tarefa_Mod8/src/Alunos.java
+++ Tarefa_Mod8/src/Alunos.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Alunos {
+/*
+* @author pmpedrolima@gmail.com
+ */
+
+public class Alunos {
+    //declaração de variaveis
+    private String nome;
+    private long matricula;
+
+    // metodos getters and setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
 }
--- Tarefa_Mod8/src/Main.java
+++ Tarefa_Mod8/src/Main.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Main {
+/*
+ * @author pmpedrolima@gmail.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        Alunos aluno = new Alunos();
+        aluno.setNome("Pedro");
+        aluno.setMatricula(211001);
+
+        Notas nota = new Notas();
+        nota.setNota1(7);
+        nota.setNota2(2);
+        nota.setNota3(4);
+        nota.setNota4(5);
+
+
+        System.out.println("Aluno: " + aluno.getNome());
+        System.out.println("De matricula " + aluno.getMatricula());
+        System.out.println("Atingiu média de: " + nota.getMedia());
+
+        if (nota.getMedia() >= 6.0) {
+            System.out.println("Aluno aprovado");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+
+        System.out.println("*********************************************");
+
+        Alunos aluno2 = new Alunos();
+        aluno2.setMatricula(211001);
+        aluno2.setNome("João");
+
+        Notas nota2 = new Notas();
+        nota2.setNota1(10);
+        nota2.setNota2(8);
+        nota2.setNota3(9);
+        nota2.setNota4(8);
+
+        System.out.println("Aluno: " + aluno2.getNome());
+        System.out.println("De matricula " + aluno2.getMatricula());
+        System.out.println("Atingiu média de: " + nota2.getMedia());
+
+        if (nota2.getMedia() >= 6.0) {
+            System.out.println("Aluno aprovado");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+
+
+    }
 }
