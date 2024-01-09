53430ffd6e9862321d94059890ba0ac4c97976a3
Pedro Matheus <92803869+pedrojoop@users.noreply.github.com>
2024-01-09 11:37:52 -0300

    Vigésima sétima tarefa

---
+++ primeiroProjetoMaven/src/main/java/com/ebac/pedro/primeiroProjetoMaven/PrimeiroProjetoMavenApplication.java
@@ -0,0 +1,13 @@
+package com.ebac.pedrp.primeiroProjetoMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoMavenApplication.class, args);
+	}
+
+}
